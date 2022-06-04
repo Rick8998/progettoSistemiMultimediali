1968a808776a638c6ac4adab2e278b9733c288ca
Rick <riccardo.cecci.rc.rc@gmail.com>
2022-06-04 10:07:07 +0200

    Rotate class implementation

--- src/main/Rotate.java
+++ src/main/Rotate.java
@@ -4,9 +4,11 @@ import java.awt.image.BufferedImage;
 
 public class Rotate implements Transform{
 	
-	BufferedImage source, result;
+	BufferedImage source, result, tmp;
 	double theta;
 	int Xc, Yc;
+	private int Xp;
+	private int Yp;
 	
 	public void setRotationAngle(double ratio) {
 		theta = ratio;
@@ -26,7 +28,40 @@ public class Rotate implements Transform{
 	@Override
 	public void calculate() {
 		// perform rotation and store results in result
-		//do to...
+		double ratio = 2.1;
+		tmp = new BufferedImage((int)(source.getWidth()*ratio), (int)(source.getHeight()*ratio), source.getType());
+		int band = result.getRaster().getNumBands();
+		for(int x = 0; x < tmp.getWidth(); x++) {
+			for(int y = 0; y < tmp.getHeight(); y++) {
+				int i = (int) (x / ratio);
+				int j = (int) (y / ratio);
+				for(int c = 0; c < band; c++) {
+					int pixel = source.getRaster().getSample(i, y, c);
+					tmp.getRaster().setSample(x, y, c, pixel);
+				}
+			}
+		}
+		
+		for(int x = tmp.getWidth()/2; x < tmp.getWidth(); x++) {
+			for(int y = tmp.getHeight()/2; y < tmp.getHeight(); y++) {
+				for(int c = 0; c < band; c++) {
+					double pixel = tmp.getRaster().getSampleDouble(x+tmp.getWidth()/2, y+tmp.getHeight()/2, c);
+					rotationMatrix(x, y);
+					Xp = (int)(Xp/ratio);
+					Yp=(int) (Yp/ratio);
+					if(Xp >= 0 && Yp >= 0 && Xp < result.getWidth() && Yp < result.getHeight() ) {
+						result.getRaster().setSample(Xp, Yp, c, pixel);
+					}
+				}
+			}			
+		}
+	}
+
+	private void rotationMatrix(int x, int y) {
+		double cos = Math.cos(theta);
+		double sin = Math.sin(theta);
+		Xp = (int) ( ( x*Math.cos(theta) ) + ( y*Math.sin(theta) ) ) + Xc;
+		Yp = (int) ( ( x* -( Math.cos(theta) ) ) + ( y*Math.sin(theta) ) ) + Yc;
 	}
 
 	@Override
