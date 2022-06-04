ff10ee8c43195e80dede2297aee4271a7a7e9d07
Rick <riccardo.cecci.rc.rc@gmail.com>
2022-06-04 10:54:47 +0200

    Added and implemented main class

---
+++ src/main/Main.java
@@ -0,0 +1,55 @@
+package main;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedImage original = null;
+		try {
+			original = ImageIO.read(new File("img/SampleCol.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		BufferedImage[] doubleBuffer = {
+				new BufferedImage(original.getWidth(), original.getHeight(), original.getType()),
+				new BufferedImage(original.getWidth(), original.getHeight(), original.getType()),
+		};
+		
+		int currentBuffer = 0;
+		
+		Rotate imgRotation = new Rotate();
+		imgRotation.setSourceData(original);
+		imgRotation.setRotationAngle(0);
+		imgRotation.setCenterPoint((int)(original.getWidth()*1.05), (int)(original.getHeight()*1.05));
+		
+		JFrame frame = new JFrame("Image rotation");
+		JLabel label = new JLabel(new ImageIcon(original));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(label);
+		frame.pack();
+		frame.setLocation(20, 20);
+		frame.setVisible(true);
+		
+		for(int i = 0; i < 100; i++) {
+			imgRotation.setRotationAngle(45*i);
+			imgRotation.calculate();
+			doubleBuffer[currentBuffer] = (BufferedImage) imgRotation.getResult();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			label.setIcon(new ImageIcon(doubleBuffer[currentBuffer]));
+			if(currentBuffer == 0) currentBuffer = 1;
+			else currentBuffer = 0;
+		}
+	}
+}
--- src/main/Rotate.java
+++ src/main/Rotate.java
@@ -22,20 +22,22 @@ public class Rotate implements Transform{
 	@Override
 	public void setSourceData(Object src) {
 		source= (BufferedImage)src;
-		result= new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+		//result= new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
 	}
 
 	@Override
 	public void calculate() {
+		result= new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
 		// perform rotation and store results in result
 		//upsampling per evitare di perdere pixel durante la rotazione
 		double ratio = 2.1;
 		tmp = new BufferedImage((int)(source.getWidth()*ratio), (int)(source.getHeight()*ratio), source.getType());
 		int band = result.getRaster().getNumBands();
-		for(int x = 0; x < tmp.getWidth(); x++) {
-			for(int y = 0; y < tmp.getHeight(); y++) {
-				int i = (int) (x / ratio);
-				int j = (int) (y / ratio);
+		int i, j, x, y;
+		for( x = 0; x < tmp.getWidth(); x++) {
+			for( y = 0; y < tmp.getHeight(); y++) {
+				 i = (int) (x / ratio);
+				 j = (int) (y / ratio);
 				for(int c = 0; c < band; c++) {
 					int pixel = source.getRaster().getSample(i, j, c);
 					tmp.getRaster().setSample(x, y, c, pixel);
@@ -44,10 +46,10 @@ public class Rotate implements Transform{
 		}
 		
 		//rotazione dell'immagine
-		for(int x = tmp.getWidth()/2; x < tmp.getWidth(); x++) {
-			for(int y = tmp.getHeight()/2; y < tmp.getHeight(); y++) {
+		for( x = tmp.getWidth()/2; x < tmp.getWidth(); x++) {
+			for( y = tmp.getHeight()/2; y < tmp.getHeight(); y++) {
 				for(int c = 0; c < band; c++) {
-					double pixel = tmp.getRaster().getSampleDouble(x+tmp.getWidth()/2, y+tmp.getHeight()/2, c);
+					double pixel = tmp.getRaster().getSample(x+(tmp.getWidth())/2, y+(tmp.getHeight())/2, c);
 					rotationMatrix(x, y);
 					Xp = (int)(Xp/ratio);
 					Yp=(int) (Yp/ratio);
@@ -62,8 +64,8 @@ public class Rotate implements Transform{
 	private void rotationMatrix(int x, int y) {
 		double cos = Math.cos(theta);
 		double sin = Math.sin(theta);
-		Xp = (int) ( ( x*Math.cos(theta) ) + ( y*Math.sin(theta) ) ) + Xc;
-		Yp = (int) ( ( x* -( Math.cos(theta) ) ) + ( y*Math.sin(theta) ) ) + Yc;
+		Xp = (int) ((x*cos) + (y*sin)) + Xc;
+		Yp = (int) ((x* -cos) + (y*sin)) + Yc;
 	}
 
 	@Override
